0c7a3d5f6a88c2db8c0c0b078e9d152b86b48d34
Steven <steven1126@swbell.net>
2020-10-06 18:53:38 -0400

    stocks url

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, StocksPageView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
+    path('finance/', StocksPageView.as_view(), name='stocks'),
     path('', HomePageView.as_view(), name='home'),
 ]
