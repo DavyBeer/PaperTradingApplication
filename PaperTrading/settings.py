1410d79ce31d97822efd18c5397f538d7cf176f8
Steven <steven1126@swbell.net>
2020-10-06 17:41:32 -0400

    INSTALLED_APPS

--- PaperTrading/settings.py
+++ PaperTrading/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
     'accounts.apps.AccountsConfig',
+    'stocks.apps.stocksConfig',
     'django_secrets',
 ]
 
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'ET'
 
 USE_I18N = True
 
