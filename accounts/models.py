5053a943d874eef05fbae219e22c93c61d3841d1
Steven <steven1126@swbell.net>
2020-10-07 15:22:35 -0400

    Deleted app

--- accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    Name = models.CharField(max_length=50)
-    Cash = models.IntegerField()
-
-    def __str__(self):
-        return self.Name
