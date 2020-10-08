40841bb184db2c5cc93e706c7d1ae1c986f59047
Steven <steven1126@swbell.net>
2020-10-08 16:00:03 -0400

    Add new url

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, StocksPageView, SignUpView
+from .views import HomePageView, AboutPageView, StocksPageView, SignUpView, TablePageView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('stocks/', StocksPageView.as_view(), name='stocks'),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('table/', TablePageView.as_view(), name='table'),
     path('', HomePageView.as_view(), name='home'),
 ]
