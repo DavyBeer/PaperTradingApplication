8e2783f905c7d3d5566a2a570cabb86ea627c8e6
Steven <steven1126@swbell.net>
2020-10-08 15:58:19 -0400

    Slight change

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Create your models here.
 class User(models.Model):
-    Name = models.CharField(max_length=50)
-    Cash = models.IntegerField()
+    Name = models.OneToOneField(User, on_delete=models.CASCADE)
+    Cash = models.IntegerField(default = 500, editable = True)
 
     def __str__(self):
         return self.Name
