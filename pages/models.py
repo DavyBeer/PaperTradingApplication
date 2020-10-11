87e9392d2a15c3e8b66219ad2c9d965da61dfd04
Steven <steven1126@swbell.net>
2020-10-10 21:23:31 -0400

    Comment out

--- pages/models.py
+++ pages/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 #Create your models here.
-class User(models.Model):
-    Name = models.OneToOneField(User, on_delete=models.CASCADE)
-    Cash = models.IntegerField(default = 500, editable = True)
+# class User(models.Model):
+#     Name = models.OneToOneField(User, on_delete=models.CASCADE)
+#     Cash = models.IntegerField(default = 500, editable = True)
 
-    def __str__(self):
-        return self.Name
+#     def __str__(self):
+#         return self.Name
