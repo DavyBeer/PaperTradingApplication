67c84fe6170f50216203aef6738987815d5757a8
Steven <steven1126@swbell.net>
2020-10-10 21:23:43 -0400

    Removed table

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, StocksPageView, SignUpView, TablePageView
+from .views import HomePageView, AboutPageView, StocksPageView, SignUpView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('stocks/', StocksPageView.as_view(), name='stocks'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('table/', TablePageView.as_view(), name='table'),
     path('', HomePageView.as_view(), name='home'),
 ]
